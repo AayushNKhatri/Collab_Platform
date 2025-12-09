9568763e3df9bd110a12f9a19cafee7919213666
AayuhNKhatri <aayush.khatri@antsware.com>
2025-12-09 17:28:37 +0545

    Fix: Get Endpoint

---
+++ Collab-Platform.ApplicationLayer/DTO/PermissionDto/EndpointDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace Collab_Platform.ApplicationLayer.DTO.PermissionDto
+{
+    public class EndpointDTO
+    {
+        public string? RoutePattern { get; set; }
+        public IReadOnlyList<string>? HttpMethod { get; set; }
+        public string? Controller { get; set; }
+        public string? Action { get; set; }
+    }
+}
